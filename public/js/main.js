4ce912825fc1cfd68b2ae75536d4db1472c2a816
Paul Schnau <paul@honey.is>
2015-07-11 13:59:02 -0400

    firebase

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,33 @@
+myLat = 40.7127;
+myLng = -74.0059;
 
+var firebase = new Firebase("https://rak360.firebaseio.com/");
 
-$(document).ready(function() {
-
-var myFirebaseRef = new Firebase("https://rak360.firebaseio.com/");
-
-});
 
 function initialize() {
         var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: { lat: myLat, lng: myLng},
+          zoom: 12
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+  	myLat = position.coords.latitude;
+  	myLng = position.coords.longitude;
+    console.log(myLat);
+    console.log(myLng);
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
+
+
+firebase.child("tasks").on("value", function(snapshot) {
+	console.log(snapshot.val());
+});
